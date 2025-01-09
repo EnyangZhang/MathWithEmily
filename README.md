10a5600ac6651d0db149371890a64fda83181232
Enyang Zhang <ezh15@uclive.ac.nz>
2025-01-09 18:28:32 +1300

    Initial commit for MathWithEmily

---
+++ README.md
@@ -0,0 +1,2 @@
+# MathWithEmily
+An engaging math practice app designed for 8-year-old Emily, built with Kotlin and Android Studio.
---
+++ app/src/main/java/nz/app/enyang/mathwithemilyaipowered/MainActivity.kt
@@ -0,0 +1,27 @@
+package nz.app.enyang.mathwithemilyaipowered
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import nz.app.enyang.mathwithemilyaipowered.ui.theme.MathWithEmilyAIPoweredTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MathWithEmilyAIPoweredTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
